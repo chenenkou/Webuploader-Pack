39826bdea07b51210858356467fdd9a5442909c2
S76 <S76hckj@ooopic.com>
2016-10-18 10:07:01 +0800

    improve pack

--- js/baiduUpload.js
+++ js/baiduUpload.js
@@ -1,4 +1,4 @@
-window.BaiduUpload = (function($, WebUploader) {
+window.BaiduUpload = (function ($, WebUploader) {
     function BaiduUpload(options, setting) {
         this.handler = null;
         this.uploader = options.uploader || null;
@@ -22,96 +22,118 @@ window.BaiduUpload = (function($, WebUploader) {
             // 上传并发数。允许同时最大上传进程数
             threads: 1,
             // 验证单个文件大小是否超出限制, 超出则不允许加入队列
-            fileSingleSizeLimit: 1024*1024*5,
+            fileSingleSizeLimit: 1024 * 1024 * 5,
             // 指定接受哪些类型的文件。 由于目前还有ext转mimeType表，所以这里需要分开指定
             accept: {
                 extensions: 'gif,jpg,jpeg,bmp,png',
                 mimeTypes: 'image/*'
             }
-
         };
 
         config = $.extend(config, options);
-
+        var that = this;
         this.setting = $.extend({
-            fileQueued: function(that, file) {
-                var $uploader = $(that.uploader);
-                var $statusBar = $uploader.find(".statusBar");
-                $statusBar.html('<div class="progress"><span class="text"></span><span class="percentage"></span></div>');
-            }
+            fileQueued: that._defaultFileQueued, // 加入队列
+            error: that._defaultError, // 验证
+            uploadProgress: that._defaultUploadProgress, // 上传中
+            uploadComplete: that._defaultUploadComplete // 完成
         }, setting);
 
         this.handler = WebUploader.create(config);
     }
 
+    // 默认方法 加入队列-验证-上传中-完成
+    BaiduUpload.prototype._defaultFileQueued = function (that, file) {
+        var $uploader = $(that.uploader);
+        var $statusBar = $uploader.find(".statusBar");
+        $statusBar.html('\
+                    <div class="progress"><span class="text"></span><span class="percentage"></span></div>\
+                ');
+    };
+    BaiduUpload.prototype._defaultError = function (type) {
+        switch (type) {
+            case "F_EXCEED_SIZE":
+                alert('文件大小超过限制');
+                break;
+            case "Q_TYPE_DENIED":
+                alert("文件格式不支持");
+                break;
+            default:
+                console.warn(type);
+                alert("未知错误");
+        }
+    };
+    BaiduUpload.prototype._defaultUploadProgress = function (that, file, percentage) {
+        var $uploader = $(that.uploader),
+            $percent = $uploader.find('.progress .percentage'),
+            $info = $uploader.find('.progress .text');
+
+        var percent = parseInt(percentage * 100);
+        $info.html(percent + '%');
+        $percent.css('width', percent + '%');
+    };
+    BaiduUpload.prototype._defaultUploadComplete = function (that, file) {
+        var $uploader = $(that.uploader);
+        var $statusBar = $uploader.find(".statusBar");
+        setTimeout(function () {
+            $statusBar.html("");
+            that.handler.reset();
+        }, 800);
+    };
 
     // 文件加入上传队列
-    BaiduUpload.prototype.fileQueued = function(callback) {
+    BaiduUpload.prototype.fileQueued = function (callback) {
+        if ($.isFunction(callback)) {
+            this.setting.fileQueued = callback;
+            return true;
+        }
         var that = this;
-        this.handler.on( 'fileQueued', function(file) {
+        this.handler.on('fileQueued', function (file) {
             that.setting.fileQueued(that, file);
         });
     };
 
     // 文件验证不通过
-    BaiduUpload.prototype.error = function() {
-        this.handler.on('error', function(type) {
-            switch (type) {
-                case "F_EXCEED_SIZE":
-                    alert('文件大小超过限制');
-                    break;
-                case "Q_TYPE_DENIED":
-                    alert("文件格式不支持");
-                    break;
-                default:
-                    console.warn(type);
-                    alert("未知错误");
-            }
+    BaiduUpload.prototype.error = function (callback) {
+        if ($.isFunction(callback)) {
+            this.setting.error = callback;
+            return true;
+        }
+        var that = this;
+        this.handler.on('error', function (type) {
+            that.setting.error(type);
         });
     };
 
     // 文件上传过程中创建进度条实时显示
-    BaiduUpload.prototype.uploadProgress = function(callback) {
+    BaiduUpload.prototype.uploadProgress = function (callback) {
+        if ($.isFunction(callback)) {
+            this.setting.uploadProgress = callback;
+            return true;
+        }
         var that = this;
-        this.handler.on( 'uploadProgress', function( file, percentage ) {
-            if ($.isFunction(callback)) {
-                callback(file, percentage);
-                return true;
-            }
-
-            var $uploader = $(that.uploader),
-                $percent = $uploader.find('.progress .percentage'),
-                $info = $uploader.find('.progress .text');
-
-            var percent = parseInt(percentage * 100);
-            $info.html(percent + '%');
-            $percent.css( 'width', percent + '%' );
+        this.handler.on('uploadProgress', function (file, percentage) {
+            that.setting.uploadProgress(that, file, percentage);
         });
     };
 
     // 文件上传完成
-    BaiduUpload.prototype.uploadComplete = function(callback) {
+    BaiduUpload.prototype.uploadComplete = function (callback) {
+        if ($.isFunction(callback)) {
+            this.setting.uploadComplete = callback;
+            return true;
+        }
         var that = this;
-        this.handler.on( 'uploadComplete', function( file ) {
-            if ($.isFunction(callback)) {
-                callback(file);
-                return true;
-            }
-
-            var $uploader = $(that.uploader);
-            var $statusBar = $uploader.find(".statusBar");
-            setTimeout(function() {
-                $statusBar.html("");
-                that.handler.reset();
-            }, 800);
+        this.handler.on('uploadComplete', function (file) {
+            that.setting.uploadComplete(that, file);
         });
     };
 
     // 文件上传成功
-    BaiduUpload.prototype.uploadSuccess = function(callback) {
+    BaiduUpload.prototype.uploadSuccess = function (callback) {
         this.handler.on('uploadSuccess', function (file, response) {
-            console.log(file);
-            console.log(response);
+            //console.log(file);
+            //console.log(response);
             if ($.isFunction(callback)) {
                 callback(file, response);
                 return true;
