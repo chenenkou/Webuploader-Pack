f097ab62349fbf4e82c9c3050e38586c2637ccd2
chenenkou <chenenkou@163.com>
2018-06-27 16:06:32 +0800

    完成上传七牛的demo

--- js/upload.js
+++ js/upload.js
@@ -1,28 +1,63 @@
 $(function() {
     var server_image_url = "/";
 
-    var options = {
-        pick: "#picker",
-        uploader: "#uploader",
-        server: "./fileupload.php"
-    };
-    var Uploader = new BaiduUpload(options);
-    Uploader.upload(function(file, response) {
+    // 上传图片
+    (function () {
+        var options = {
+            pick: "#picker",
+            uploader: "#uploader",
+            accept: {
+                extensions: 'gif,jpg,jpeg,bmp,png',
+                mimeTypes: 'image/gif,image/jpg,image/jpeg,image/bmp,image/png'
+            },
+            server : window.location.protocol + '//upload.qiniup.com/',
+            formData: {
+                token : "" // 七牛上传的TOKEN
+            }
+        };
+        var Uploader = new BaiduUpload(options);
+        Uploader.upload(function(file, response) {
+            // response = {"domain": "https://pic2.hanmaker.com/", "key":"common/images/20180627/image/png/24541/Fq8PAPrzaE8Nd2eA8i34VNiKO9I_153715.png"}
+            var $uploader = $(options.uploader);
+            var $img = $uploader.find("img");
+            var src = response.domain + response.key;
+            $img.attr("src", src);
+        });
+    })();
 
-        var $uploader = $(options.uploader);
-        var $img = $uploader.find("img");
-        $img.attr("src", server_image_url + response.result);
-    });
+    // 上传文件
+    (function () {
+        var options2 = {
+            pick: "#picker2",
+            uploader: "#uploader2",
+            accept: {
+                extensions: 'zip',
+                mimeTypes: '"application/x-zip-compressed"'
+            },
+            server : window.location.protocol + '//upload.qiniup.com/',
+            formData: {
+                token : "" // 七牛上传的TOKEN
+            }
+        };
+        var setting2 = {
+            fileQueued: function(that, file) {
+                var $uploader = $(that.uploader);
+            },
+            // 上传中进度处理
+            uploadProgress: function (that, file, percentage) {
+                var percent = parseInt(percentage * 100);
+                console.log(percent);
+            }
+        }
+        var Uploader2 = new BaiduUpload(options2, setting2);
+        Uploader2.upload(function(file, response) {
+            // response = {"domain": "https://pic2.hanmaker.com/", "name": "abc.zip", "key":"common/images/20180627/image/png/24541/Fq8PAPrzaE8Nd2eA8i34VNiKO9I_153715.png"}
+            var $uploader = $(options2.uploader);
+            var $a = $uploader.find("a");
+            var href = response.domain + response.key;
+            $a.attr("href", href);
+            $a.text(response.name);
+        });
+    })();
 
-    var options2 = {
-        pick: "#picker2",
-        uploader: "#uploader2",
-        server: "./fileupload.php"
-    };
-    var Uploader2 = new BaiduUpload(options2);
-    Uploader2.upload(function(file, response) {
-        var $uploader = $(options2.uploader);
-        var $img = $uploader.find("img");
-        $img.attr("src", server_image_url + response.result);
-    });
 });
